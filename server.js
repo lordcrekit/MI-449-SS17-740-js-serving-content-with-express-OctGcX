64e63c65d23b8f7bf9432a708539ba859bf32c3b
William Norman <lordcrekit@gmail.com>
2017-04-25 15:23:35 -0400

    No longer uses router+staticly loads jsonfile

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-var jsonfile = require('jsonfile')
 var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080
@@ -7,33 +6,32 @@ app.set('view_engine', 'ejs')
 
 app.use(express.static('public'))
 
+const CATS_DATA = require('./data/cats.json')
+
+// Serve homepage
 app.get('/', function (req, res) {
-  jsonfile.readFile(CATS_DATA, function (err, obj) {
-    if (err) { res.send(err); return }
-    let keys = Object.keys(obj.cats)
-    let cat = keys[Math.floor(Math.random() * keys.length)]
-    res.render('index.ejs', {title: 'CATS', catname: cat, cat: obj.cats[cat]})
-  })
+  let keys = Object.keys(CATS_DATA.cats)
+  let cat = keys[Math.floor(Math.random() * keys.length)]
+  res.render('index.ejs',
+    {title: 'CATS', catname: cat, cat: CATS_DATA.cats[cat]})
 })
 
-const CATS_DATA = 'data/cats.json'
-var catrouter = express.Router()
-
-catrouter.get('', function (req, res) {
-  jsonfile.readFile(CATS_DATA, function (err, obj) {
-    if (err) { res.send(err); return }
-    res.render('allcats.ejs', {title: 'ALL CATS xD', cats: obj.cats})
-  })
+// Serve all cats
+app.get('/cat', function (req, res) {
+  res.render('allcats.ejs', {title: 'ALL CATS xD', cats: CATS_DATA.cats})
 })
 
-catrouter.get('/*', function (req, res) {
-  var cat = req.url.slice(1)
-  jsonfile.readFile(CATS_DATA, function (err, obj) {
-    if (err) { res.send(err); return }
-    res.render('onecat.ejs', {title: cat, catname: cat, cat: obj.cats[cat]})
-  })
+// Serve single cat
+app.get('/cat/:catname', function (req, res) {
+  var catname = req.params.catname
+  var cat = CATS_DATA.cats[catname]
+  if (!cat) { res.send('404'); return }
+  res.render('onecat.ejs', {title: catname, catname: catname, cat: cat})
 })
 
-app.use('/cat', catrouter)
+// Handle 404
+app.get('*', function (req, res) {
+  res.send('404')
+})
 
 app.listen(port)
